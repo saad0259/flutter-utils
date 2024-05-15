4dafa7ecfcd5699f3e245847c701d712a82d84e5
Saad Shafiq <saadshafiq259@gmail.com>
2024-05-15 19:38:23 +0500

    comment non-essential files

--- api_helper.dart
+++ api_helper.dart
@@ -1,84 +1,84 @@
-import 'dart:developer';
+// import 'dart:developer';
 
-import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:dio/dio.dart';
+// import 'package:flutter/foundation.dart';
 
-// * Dio Start
-enum Method { GET, POST, PATCH, DELETE }
+// // * Dio Start
+// enum Method { GET, POST, PATCH, DELETE }
 
-class Request {
-  final String _url;
-  final dynamic _body;
+// class Request {
+//   final String _url;
+//   final dynamic _body;
 
-  Request(
-    this._url,
-    this._body,
-  );
+//   Request(
+//     this._url,
+//     this._body,
+//   );
 
-  Future<Response<dynamic>> _sendRequest(Method method, String baseUrl) async {
-    final dio = DioSingleton.instance.dio;
-    try {
-      return await dio.request(
-        baseUrl + _url,
-        options: Options(method: _getMethodString(method)),
-        data: _body,
-      );
-    } catch (e) {
-      log('Dio Error: $e');
-      return Future.error(e);
-    }
-  }
+//   Future<Response<dynamic>> _sendRequest(Method method, String baseUrl) async {
+//     final dio = DioSingleton.instance.dio;
+//     try {
+//       return await dio.request(
+//         baseUrl + _url,
+//         options: Options(method: _getMethodString(method)),
+//         data: _body,
+//       );
+//     } catch (e) {
+//       log('Dio Error: $e');
+//       return Future.error(e);
+//     }
+//   }
 
-  String _getMethodString(Method method) {
-    switch (method) {
-      case Method.GET:
-        return 'GET';
-      case Method.POST:
-        return 'POST';
-      case Method.PATCH:
-        return 'PATCH';
-      case Method.DELETE:
-        return 'DELETE';
-    }
-  }
+//   String _getMethodString(Method method) {
+//     switch (method) {
+//       case Method.GET:
+//         return 'GET';
+//       case Method.POST:
+//         return 'POST';
+//       case Method.PATCH:
+//         return 'PATCH';
+//       case Method.DELETE:
+//         return 'DELETE';
+//     }
+//   }
 
-  Future<Response> get(String baseUrl) => _sendRequest(Method.GET, baseUrl);
+//   Future<Response> get(String baseUrl) => _sendRequest(Method.GET, baseUrl);
 
-  Future<Response> post(String baseUrl) => _sendRequest(Method.POST, baseUrl);
+//   Future<Response> post(String baseUrl) => _sendRequest(Method.POST, baseUrl);
 
-  Future<Response> patch(String baseUrl) => _sendRequest(Method.PATCH, baseUrl);
+//   Future<Response> patch(String baseUrl) => _sendRequest(Method.PATCH, baseUrl);
 
-  Future<Response> delete(String baseUrl) =>
-      _sendRequest(Method.DELETE, baseUrl);
-}
+//   Future<Response> delete(String baseUrl) =>
+//       _sendRequest(Method.DELETE, baseUrl);
+// }
 
-class DioSingleton {
-  static final DioSingleton _instance = DioSingleton._internal();
-  late Dio dio;
-  static DioSingleton get instance => _instance;
+// class DioSingleton {
+//   static final DioSingleton _instance = DioSingleton._internal();
+//   late Dio dio;
+//   static DioSingleton get instance => _instance;
 
-  DioSingleton._internal() {
-    const Duration timeout = Duration(seconds: 30);
-    dio = Dio(BaseOptions(
-      responseType: ResponseType.json,
-      connectTimeout: timeout,
-      receiveTimeout: timeout,
-    ));
-  }
-}
+//   DioSingleton._internal() {
+//     const Duration timeout = Duration(seconds: 30);
+//     dio = Dio(BaseOptions(
+//       responseType: ResponseType.json,
+//       connectTimeout: timeout,
+//       receiveTimeout: timeout,
+//     ));
+//   }
+// }
 
-// * Dio End
+// // * Dio End
 
-Future<T> executeSafely<T>(Future<T> Function() function) async {
-  try {
-    return await function();
-  } on DioException catch (e) {
-    // debugPrint(e.response?.data.toString());
-    log(e.response?.data.toString() ?? '');
-    // log('Dio Error: ${e.response?.data['msg']}');
-    rethrow;
-  } catch (e) {
-    debugPrint(e.toString());
-    rethrow;
-  }
-}
+// Future<T> executeSafely<T>(Future<T> Function() function) async {
+//   try {
+//     return await function();
+//   } on DioException catch (e) {
+//     // debugPrint(e.response?.data.toString());
+//     log(e.response?.data.toString() ?? '');
+//     // log('Dio Error: ${e.response?.data['msg']}');
+//     rethrow;
+//   } catch (e) {
+//     debugPrint(e.toString());
+//     rethrow;
+//   }
+// }
--- snippets/shimmers_effects.dart
+++ snippets/shimmers_effects.dart
@@ -1,127 +1,127 @@
-import 'package:flutter/material.dart';
-import 'package:shimmer/shimmer.dart';
+// import 'package:flutter/material.dart';
+// import 'package:shimmer/shimmer.dart';
 
-final Color shimmerbaseColor = Colors.grey[300]!;
-final Color shimmerHighlightColor = Colors.grey[100]!;
+// final Color shimmerbaseColor = Colors.grey[300]!;
+// final Color shimmerHighlightColor = Colors.grey[100]!;
 
-Widget getShimmer({
-  double height = 12,
-  double width = 40,
-  double itemElevation = 0,
-  double itemBorderRadius = 5,
-}) {
-  return Shimmer.fromColors(
-    baseColor: shimmerbaseColor,
-    highlightColor: shimmerHighlightColor,
-    child: Card(
-      elevation: itemElevation,
-      clipBehavior: Clip.hardEdge,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(itemBorderRadius),
-      ),
-      child: SizedBox(
-        height: height,
-        width: width,
-      ),
-    ),
-  );
-}
+// Widget getShimmer({
+//   double height = 12,
+//   double width = 40,
+//   double itemElevation = 0,
+//   double itemBorderRadius = 5,
+// }) {
+//   return Shimmer.fromColors(
+//     baseColor: shimmerbaseColor,
+//     highlightColor: shimmerHighlightColor,
+//     child: Card(
+//       elevation: itemElevation,
+//       clipBehavior: Clip.hardEdge,
+//       shape: RoundedRectangleBorder(
+//         borderRadius: BorderRadius.circular(itemBorderRadius),
+//       ),
+//       child: SizedBox(
+//         height: height,
+//         width: width,
+//       ),
+//     ),
+//   );
+// }
 
-Widget shimmerHorizontalList({
-  double itemSpacing = 30,
-  double itemHeight = 150,
-  double itemWidth = 250,
-  int itemCount = 10,
-  double itemBorderRadius = 5,
-}) {
-  return ListView.separated(
-    scrollDirection: Axis.horizontal,
-    shrinkWrap: true,
-    physics: const NeverScrollableScrollPhysics(),
-    clipBehavior: Clip.hardEdge,
-    itemCount: itemCount,
-    separatorBuilder: (BuildContext context, int index) {
-      return SizedBox(
-        width: itemSpacing,
-      );
-    },
-    itemBuilder: (context, index) {
-      return getShimmer(
-        height: itemHeight,
-        width: itemWidth,
-        itemBorderRadius: itemBorderRadius,
-      );
-    },
-  );
-}
+// Widget shimmerHorizontalList({
+//   double itemSpacing = 30,
+//   double itemHeight = 150,
+//   double itemWidth = 250,
+//   int itemCount = 10,
+//   double itemBorderRadius = 5,
+// }) {
+//   return ListView.separated(
+//     scrollDirection: Axis.horizontal,
+//     shrinkWrap: true,
+//     physics: const NeverScrollableScrollPhysics(),
+//     clipBehavior: Clip.hardEdge,
+//     itemCount: itemCount,
+//     separatorBuilder: (BuildContext context, int index) {
+//       return SizedBox(
+//         width: itemSpacing,
+//       );
+//     },
+//     itemBuilder: (context, index) {
+//       return getShimmer(
+//         height: itemHeight,
+//         width: itemWidth,
+//         itemBorderRadius: itemBorderRadius,
+//       );
+//     },
+//   );
+// }
 
-Shimmer shimmerTableEffect({
-  EdgeInsetsGeometry padding = const EdgeInsets.all(0),
-  double itemHeight = 40,
-  int itemCount = 20,
-  double itemBorderRadius = 20,
-}) {
-  return Shimmer.fromColors(
-    baseColor: shimmerbaseColor,
-    highlightColor: shimmerHighlightColor,
-    child: Padding(
-      padding: padding,
-      child: ListView.separated(
-        physics: const NeverScrollableScrollPhysics(),
-        shrinkWrap: true,
-        itemCount: itemCount,
-        separatorBuilder: (BuildContext context, int index) {
-          return const Divider(
-            endIndent: 0,
-            color: Colors.black,
-            height: 0,
-            indent: 0,
-          );
-        },
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(itemBorderRadius)),
-            ),
-            child: SizedBox(
-              height: itemHeight,
-            ),
-          );
-        },
-      ),
-    ),
-  );
-}
+// Shimmer shimmerTableEffect({
+//   EdgeInsetsGeometry padding = const EdgeInsets.all(0),
+//   double itemHeight = 40,
+//   int itemCount = 20,
+//   double itemBorderRadius = 20,
+// }) {
+//   return Shimmer.fromColors(
+//     baseColor: shimmerbaseColor,
+//     highlightColor: shimmerHighlightColor,
+//     child: Padding(
+//       padding: padding,
+//       child: ListView.separated(
+//         physics: const NeverScrollableScrollPhysics(),
+//         shrinkWrap: true,
+//         itemCount: itemCount,
+//         separatorBuilder: (BuildContext context, int index) {
+//           return const Divider(
+//             endIndent: 0,
+//             color: Colors.black,
+//             height: 0,
+//             indent: 0,
+//           );
+//         },
+//         itemBuilder: (BuildContext context, int index) {
+//           return Card(
+//             shape: RoundedRectangleBorder(
+//               borderRadius: BorderRadius.all(Radius.circular(itemBorderRadius)),
+//             ),
+//             child: SizedBox(
+//               height: itemHeight,
+//             ),
+//           );
+//         },
+//       ),
+//     ),
+//   );
+// }
 
-Shimmer shimmerGridEffect({
-  EdgeInsetsGeometry padding = const EdgeInsets.all(0),
-  double itemAspectRatio = 1,
-  int itemCount = 20,
-  double itemBorderRadius = 5,
-  int crossAxisCount = 2,
-  bool shrinkWrap = true,
-}) {
-  return Shimmer.fromColors(
-    baseColor: shimmerbaseColor,
-    highlightColor: shimmerHighlightColor,
-    child: Padding(
-      padding: padding,
-      child: GridView.builder(
-        shrinkWrap: shrinkWrap,
-        itemCount: itemCount,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: crossAxisCount,
-          childAspectRatio: itemAspectRatio,
-        ),
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(itemBorderRadius),
-            ),
-            child: const SizedBox(),
-          );
-        },
-      ),
-    ),
-  );
-}
+// Shimmer shimmerGridEffect({
+//   EdgeInsetsGeometry padding = const EdgeInsets.all(0),
+//   double itemAspectRatio = 1,
+//   int itemCount = 20,
+//   double itemBorderRadius = 5,
+//   int crossAxisCount = 2,
+//   bool shrinkWrap = true,
+// }) {
+//   return Shimmer.fromColors(
+//     baseColor: shimmerbaseColor,
+//     highlightColor: shimmerHighlightColor,
+//     child: Padding(
+//       padding: padding,
+//       child: GridView.builder(
+//         shrinkWrap: shrinkWrap,
+//         itemCount: itemCount,
+//         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//           crossAxisCount: crossAxisCount,
+//           childAspectRatio: itemAspectRatio,
+//         ),
+//         itemBuilder: (BuildContext context, int index) {
+//           return Card(
+//             shape: RoundedRectangleBorder(
+//               borderRadius: BorderRadius.circular(itemBorderRadius),
+//             ),
+//             child: const SizedBox(),
+//           );
+//         },
+//       ),
+//     ),
+//   );
+// }
