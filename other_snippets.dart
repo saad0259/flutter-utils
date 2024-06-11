cf124a02e6e6390edf214d675c8400c7494b4be9
Saad Shafiq <saadshafiq259@gmail.com>
2024-06-11 17:14:30 +0500

    added more snippets to other_snippets

--- other_snippets.dart
+++ other_snippets.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+
+import './theme/theme.dart';
+
 // import 'package:connectivity_plus/connectivity_plus.dart';
 // import 'package:url_launcher/url_launcher.dart';
 
@@ -60,4 +64,88 @@
 
 //   // If the loop completes without returning, the versions are equal
 //   return false;
+// }
+
+class CustomRow extends StatelessWidget {
+  const CustomRow({super.key, required this.children, this.spacer = 0});
+  final List<Widget> children;
+
+  final double spacer;
+
+  @override
+  Widget build(BuildContext context) {
+    return context.isPhone
+        ? Column(
+            children: children
+                .expand((e) => [
+                      e,
+                      SizedBox(
+                        height: spacer,
+                      )
+                    ])
+                .toList()
+              ..removeLast(),
+          )
+        : Row(
+            children: children
+                .expand((e) => [
+                      Expanded(child: e),
+                      SizedBox(
+                        width: spacer,
+                      )
+                    ])
+                .toList()
+              ..removeLast(),
+          );
+  }
+}
+
+// Future<void> downloadWidgetIntoImage(GlobalKey key) async {
+//   try {
+//     RenderRepaintBoundary boundary =
+//         key.currentContext!.findRenderObject() as RenderRepaintBoundary;
+//     ui.Image image = await boundary.toImage(pixelRatio: 3.0);
+//     ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+//     Uint8List pngBytes = byteData!.buffer.asUint8List();
+//     final blob = html.Blob([pngBytes]);
+//     final url = html.Url.createObjectUrlFromBlob(blob);
+//     await FileSaver.instance.saveFile(
+//         name: '${DateTime.now().toString()}.png',
+//         bytes: pngBytes,
+//         mimeType: MimeType.png,
+//         ext: 'png');
+
+//     html.Url.revokeObjectUrl(url);
+//   } catch (e) {
+//     print(e.toString());
+//     rethrow;
+//   }
+// }
+
+// void copyToClipboard(BuildContext context, String text) {
+//   final textarea = html.TextAreaElement();
+//   html.document.body?.append(textarea);
+//   textarea.style.border = '0';
+//   textarea.style.margin = '0';
+//   textarea.style.padding = '0';
+//   textarea.style.opacity = '0';
+//   textarea.style.position = 'absolute';
+//   textarea.readOnly = true;
+//   textarea.value = text;
+//   textarea.select();
+//   html.document.execCommand('copy');
+//   textarea.remove();
+//   snack(context, 'Copied to clipboard', info: true);
+// }
+
+// String parseDate(DateTime date) {
+//   return DateFormat.yMMMd().format(date);
+// }
+
+// Future<String> uploadImage(
+//     {required Reference storage,
+//     required String name,
+//     required Uint8List image}) async {
+//   final resp = await storage.child('$name.png').putData(image);
+//   return resp.ref.getDownloadURL();
 // }
