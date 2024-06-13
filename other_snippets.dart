dc2c9381201ba4f8f983ff8308ea1160f58accec
Saad Shafiq <saadshafiq259@gmail.com>
2024-06-13 23:57:11 +0500

    added more general snippets, need formatting

--- other_snippets.dart
+++ other_snippets.dart
@@ -1,9 +1,88 @@
+import 'dart:math';
+
+import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+// import 'package:connectivity_plus/connectivity_plus.dart';
 
 import './theme/theme.dart';
 
-// import 'package:connectivity_plus/connectivity_plus.dart';
-// import 'package:url_launcher/url_launcher.dart';
+Widget getErrorMessage(BuildContext context, e) {
+  debugPrint(e);
+
+  return Center(
+    child: Text(
+      e is FirebaseException ? e.message ?? e.code : e,
+      style:
+          Theme.of(context).textTheme.titleLarge!.copyWith(color: Colors.red),
+      textAlign: TextAlign.center,
+    ),
+  );
+}
+
+Future<void> customLaunch(String receiptUrl) async {
+  final Uri url = Uri.parse(receiptUrl);
+  if (await canLaunchUrl(url)) {
+    await launchUrl(url);
+  } else {
+    print(' could not launch $url');
+  }
+}
+
+void alert(BuildContext context, String message,
+    {bool info = false, IconData? icon, String? title}) {
+  debugPrint(message);
+
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      contentPadding: const EdgeInsets.symmetric(vertical: 40, horizontal: 120),
+      actionsPadding:
+          const EdgeInsets.symmetric(vertical: 40, horizontal: 120) -
+              const EdgeInsets.only(top: 40),
+      actionsAlignment: MainAxisAlignment.center,
+      title: info
+          ? Icon(
+              icon ?? Icons.check_circle_outline,
+              color: Theme.of(context).primaryColor,
+              size: 90,
+            )
+          : Icon(
+              icon ?? Icons.error_outline,
+              color: Theme.of(context).colorScheme.error,
+              size: 90,
+            ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            title ?? (info ? "success" : "error").tr(),
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          const SizedBox(height: 16),
+          Text(
+            message,
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        ElevatedButton(
+          child: const Text("ok").tr(),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ],
+    ),
+  );
+}
+
+String getRandomNumber() {
+  var rng = Random();
+
+  return (rng.nextInt(90000) + 10000).toString();
+}
 
 // Future<bool> isInternetAvailable() async {
 //   var connectivityResult = await (Connectivity().checkConnectivity());
@@ -13,15 +92,6 @@ import './theme/theme.dart';
 //     return true;
 //   }
 //   return false;
-// }
-
-// Future<void> customLaunch(String receiptUrl) async {
-//   final Uri url = Uri.parse(receiptUrl);
-//   if (await canLaunchUrl(url)) {
-//     await launchUrl(url);
-//   } else {
-//     print(' could not launch $url');
-//   }
 // }
 
 // Future<bool> appVersionDiscontinued(String minimumVersion) async {
@@ -67,7 +137,7 @@ import './theme/theme.dart';
 // }
 
 class CustomRow extends StatelessWidget {
-  const CustomRow({super.key, required this.children, this.spacer = 0});
+  const CustomRow({Key? key, required this.children, this.spacer = 0});
   final List<Widget> children;
 
   final double spacer;
@@ -149,3 +219,6 @@ class CustomRow extends StatelessWidget {
 //   final resp = await storage.child('$name.png').putData(image);
 //   return resp.ref.getDownloadURL();
 // }
+
+Widget cellWidget(BuildContext context, String title) =>
+    Text(title, style: context.textTheme.bodyMedium);
