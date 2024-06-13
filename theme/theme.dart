282ae8d1d87bb6d52d30ea2b5fac835034907537
Saad Shafiq <saadshafiq259@gmail.com>
2024-06-13 23:58:21 +0500

    removed constant colors

--- theme/theme.dart
+++ theme/theme.dart
@@ -70,17 +70,6 @@ extension ContextExtensions on BuildContext {
   double get statusBarHeight => mediaQuery.padding.top;
   double get bottomBarHeight => mediaQuery.padding.bottom;
 
-  //* Default Colors
-  // Color get appColorGreen => const Color(0xff317020);
-  // Color get appColorRed => const Color(0xffe74c3c);
-  // Color get appColorBlue => const Color(0xff204051);
-  // Color get appColorSubText => const Color(0xff4C5264);
-  // Color get appColorDisabledButton => const Color(0xffE4E4E4);
-  // Color get appColorGrey => const Color(0XFF6C6C6C);
-  // Color get appColorBackground => const Color(0xFFF2F5FA);
-  // Color get appColorWhite => const Color(0xFFFFFFFF);
-  // Color get appColorBlack => const Color(0XFF2e2e2e);
-
   // * Default Sizes
   double get elevation => 3.0;
   double get borderRadius => 20.0;
