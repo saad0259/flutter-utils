6bb2f9e412fb7b227bf14c4aa6973eca11e90349
Saad Shafiq <saadshafiq259@gmail.com>
2024-06-13 23:57:49 +0500

    fixed image helper class

--- helpers/image_helper.dart
+++ helpers/image_helper.dart
@@ -1,4 +1,6 @@
-// import 'package:cached_network_image/cached_network_image.dart';
+// import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
 
 enum ImageType { network, file, asset, unknown }
 
@@ -17,124 +19,124 @@ extension ImageTypeExtension on ImageType {
   }
 }
 
-// class CustomImageHelper {
-//   ImageType getImageType(String imagePath) {
-//     try {
-//       final String leftSubstring = imagePath.substring(0, 8).toLowerCase();
+class CustomImageHelper {
+  ImageType getImageType(String imagePath) {
+    try {
+      final String leftSubstring = imagePath.substring(0, 8).toLowerCase();
 
-//       if (leftSubstring.contains('http')) {
-//         return ImageType.network;
-//       } else if (leftSubstring.contains('/data')) {
-//         return ImageType.file;
-//       } else if (leftSubstring.contains('asset')) {
-//         return ImageType.asset;
-//       } else {
-//         return ImageType.unknown;
-//       }
-//     } catch (e) {
-//       print('Image extension error : $e');
-//       return ImageType.unknown;
-//     }
-//   }
+      if (leftSubstring.contains('http')) {
+        return ImageType.network;
+      } else if (leftSubstring.contains('/data')) {
+        return ImageType.file;
+      } else if (leftSubstring.contains('asset')) {
+        return ImageType.asset;
+      } else {
+        return ImageType.unknown;
+      }
+    } catch (e) {
+      print('Image extension error : $e');
+      return ImageType.unknown;
+    }
+  }
+}
 
-//   class CustomNetworkImage extends StatelessWidget {
-//   const CustomNetworkImage(
-//     this.source, {
-//     Key? key,
-//     this.fit,
-//     this.errorWidget,
-//     this.width,
-//     this.height,
-//     this.alignment,
-//   }) : super(key: key);
-//   final String source;
-//   final BoxFit? fit;
-//   final Widget? errorWidget;
-//   final double? width;
-//   final double? height;
-//   final Alignment? alignment;
+class CustomNetworkImage extends StatelessWidget {
+  const CustomNetworkImage(
+    this.source, {
+    Key? key,
+    this.fit,
+    this.errorWidget,
+    this.width,
+    this.height,
+    this.alignment,
+  }) : super(key: key);
+  final String source;
+  final BoxFit? fit;
+  final Widget? errorWidget;
+  final double? width;
+  final double? height;
+  final Alignment? alignment;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return CachedNetworkImage(
-//       imageUrl: source,
-//       fit: fit ?? BoxFit.cover,
-//       width: width,
-//       height: height,
-//       alignment: alignment ?? Alignment.center,
-//       errorWidget: (context, url, error) =>
-//           errorWidget ?? const Icon(Icons.error),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      source,
+      fit: fit ?? BoxFit.cover,
+      width: width,
+      height: height,
+      alignment: alignment ?? Alignment.center,
+      errorBuilder: (context, error, stackTrace) =>
+          errorWidget ?? const Icon(Icons.error),
+    );
+  }
+}
 
-  // Future<FunctionResponse> pickUserImage(BuildContext context) async {
-  //   FunctionResponse _fResponse = getIt<FunctionResponse>();
-  //   final ThemeData theme = Theme.of(context);
-  //   String _pickedImage = '';
-  //   final ImagePicker _picker = ImagePicker();
-  //   await showDialog(
-  //       context: context,
-  //       builder: (ctx) => AlertDialog(
-  //             title: Text(
-  //               'Select an Image',
-  //               style: theme.textTheme.headline3,
-  //             ),
-  //             actions: [
-  //               TextButton(
-  //                   onPressed: () async {
-  //                     final XFile? image = await _picker.pickImage(
-  //                         source: ImageSource.gallery,
-  //                         imageQuality: 50,
-  //                         maxWidth: 150);
-  //                     if (image == null) {
-  //                       _pickedImage = '';
+// Future<FunctionResponse> pickUserImage(BuildContext context) async {
+//   FunctionResponse _fResponse = getIt<FunctionResponse>();
+//   final ThemeData theme = Theme.of(context);
+//   String _pickedImage = '';
+//   final ImagePicker _picker = ImagePicker();
+//   await showDialog(
+//       context: context,
+//       builder: (ctx) => AlertDialog(
+//             title: Text(
+//               'Select an Image',
+//               style: theme.textTheme.headlineMedium,
+//             ),
+//             actions: [
+//               TextButton(
+//                   onPressed: () async {
+//                     final XFile? image = await _picker.pickImage(
+//                         source: ImageSource.gallery,
+//                         imageQuality: 50,
+//                         maxWidth: 150);
+//                     if (image == null) {
+//                       _pickedImage = '';
 
-  //                       _fResponse.message = 'Image not picked';
+//                       _fResponse.message = 'Image not picked';
 
-  //                       Navigator.of(context).pop();
-  //                     } else {
-  //                       _pickedImage = image.path;
+//                       Navigator.of(context).pop();
+//                     } else {
+//                       _pickedImage = image.path;
 
-  //                       _fResponse.passed();
-  //                       _fResponse.data = _pickedImage;
-  //                       _fResponse.message = 'Image picked From gallery';
+//                       _fResponse.passed();
+//                       _fResponse.data = _pickedImage;
+//                       _fResponse.message = 'Image picked From gallery';
 
-  //                       Navigator.of(context).pop();
-  //                     }
-  //                   },
-  //                   child: Text(
-  //                     'Open Gallery',
-  //                     style: theme.textTheme.headline4,
-  //                   )),
-  //               TextButton(
-  //                   onPressed: () async {
-  //                     final XFile? photo =
-  //                         await _picker.pickImage(source: ImageSource.camera);
-  //                     if (photo == null) {
-  //                       _pickedImage = '';
+//                       Navigator.of(context).pop();
+//                     }
+//                   },
+//                   child: Text(
+//                     'Open Gallery',
+//                     style: theme.textTheme.headlineSmall,
+//                   )),
+//               TextButton(
+//                   onPressed: () async {
+//                     final XFile? photo =
+//                         await _picker.pickImage(source: ImageSource.camera);
+//                     if (photo == null) {
+//                       _pickedImage = '';
 
-  //                       _fResponse.message = 'Image not captured';
+//                       _fResponse.message = 'Image not captured';
 
-  //                       Navigator.of(context).pop();
-  //                       return;
-  //                     } else {
-  //                       _pickedImage = photo.path;
+//                       Navigator.of(context).pop();
+//                       return;
+//                     } else {
+//                       _pickedImage = photo.path;
 
-  //                       _fResponse.passed();
-  //                       _fResponse.data = _pickedImage;
-  //                       _fResponse.message = 'Image captured with Camera';
+//                       _fResponse.passed();
+//                       _fResponse.data = _pickedImage;
+//                       _fResponse.message = 'Image captured with Camera';
 
-  //                       Navigator.of(context).pop();
-  //                     }
-  //                   },
-  //                   child: Text(
-  //                     'Capture',
-  //                     style: theme.textTheme.headline4,
-  //                   )),
-  //             ],
-  //           ));
+//                       Navigator.of(context).pop();
+//                     }
+//                   },
+//                   child: Text(
+//                     'Capture',
+//                     style: theme.textTheme.headlineSmall,
+//                   )),
+//             ],
+//           ));
 
-  //   return _fResponse;
-  // }
+//   return _fResponse;
 // }
