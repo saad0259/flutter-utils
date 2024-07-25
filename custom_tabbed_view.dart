e89345cb00fdc4f765fa07912637bdddebb3b51a
Saad Shafiq <saadshafiq259@gmail.com>
2024-07-25 22:56:07 +0500

    relative reference

--- custom_tabbed_view.dart
+++ custom_tabbed_view.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import '../../flutter-utils/theme/theme.dart';
 import 'package:flutter/material.dart';
-
-import 'package:vida/flutter-utils/theme/theme.dart';
 
 import 'snippets/reusable_widgets.dart';
 
