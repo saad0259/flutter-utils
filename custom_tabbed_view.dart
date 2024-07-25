7ca825ba9b555b96f4e812b2742a772beafa3f16
Saad Shafiq <saadshafiq259@gmail.com>
2024-07-25 17:52:48 +0500

    edited custom tabbed view

--- custom_tabbed_view.dart
+++ custom_tabbed_view.dart
@@ -1,23 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+
+import 'package:vida/flutter-utils/theme/theme.dart';
 
 import 'snippets/reusable_widgets.dart';
 
 class CustomTabbedButton extends StatefulWidget {
   const CustomTabbedButton({
-    super.key,
+    Key? key,
     required this.items,
     required this.onChanged,
     required this.initialIndex,
-    this.unselectedColor,
+    this.showIcon = true,
     this.selectedIcon,
-  });
+    this.unselectedColor,
+    this.backgroundColor,
+  }) : super(key: key);
 
   final List<CustomTabItem> items;
   final Function(int) onChanged;
   final int initialIndex;
 
+  final bool showIcon;
   final IconData? selectedIcon;
   final Color? unselectedColor;
+  final Color? backgroundColor;
 
   @override
   State<CustomTabbedButton> createState() => _CustomTabbedButtonState();
@@ -51,27 +58,45 @@ class _CustomTabbedButtonState extends State<CustomTabbedButton>
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
 
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        SpacedRow(
-          spacing: 8,
-          children: widget.items
-              .map((e) => Expanded(
-                    child: TweenAnimationBuilder<Color?>(
-                      tween: ColorTween(
-                        begin: Colors.white,
-                        end: _selectedIndex == widget.items.indexOf(e)
-                            ? theme.colorScheme.primary
-                            : widget.unselectedColor ??
-                                ((theme.brightness == Brightness.light)
-                                    ? Colors.white
-                                    : theme.cardColor),
-                      ),
-                      duration: _animationController.duration!,
-                      builder: (context, color, child) {
-                        return TextButton.icon(
+    return Container(
+      margin: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.all(1),
+      decoration: BoxDecoration(
+        color: widget.backgroundColor,
+        borderRadius: BorderRadius.circular(context.borderRadius),
+      ),
+      child: SpacedRow(
+        spacing: 8,
+        children: widget.items
+            .map((e) => Expanded(
+                  child: TweenAnimationBuilder<Color?>(
+                    tween: ColorTween(
+                      begin: Colors.white,
+                      end: _selectedIndex == widget.items.indexOf(e)
+                          ? theme.colorScheme.primary
+                          : widget.unselectedColor ??
+                              ((theme.brightness == Brightness.light)
+                                  ? Colors.white
+                                  : theme.cardColor),
+                    ),
+                    duration: _animationController.duration!,
+                    builder: (context, color, child) {
+                      return Container(
+                        height: 35,
+                        clipBehavior: Clip.hardEdge,
+                        decoration: BoxDecoration(
+                          borderRadius:
+                              BorderRadius.circular(context.borderRadius),
+                        ),
+                        padding: EdgeInsets.zero,
+                        margin: EdgeInsets.zero,
+                        child: TextButton.icon(
                           style: TextButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                context.borderRadius,
+                              ),
+                            ),
                             backgroundColor: color,
                             foregroundColor:
                                 _selectedIndex == widget.items.indexOf(e)
@@ -79,7 +104,6 @@ class _CustomTabbedButtonState extends State<CustomTabbedButton>
                                     : theme.brightness == Brightness.light
                                         ? Colors.black
                                         : Colors.white,
-                            padding: const EdgeInsets.symmetric(vertical: 8.0),
                           ),
                           onPressed: () {
                             setState(() {
@@ -90,17 +114,21 @@ class _CustomTabbedButtonState extends State<CustomTabbedButton>
                             widget.onChanged(widget.items.indexOf(e));
                           },
                           label: child!,
-                          icon: _selectedIndex == widget.items.indexOf(e)
+                          icon: (widget.showIcon &&
+                                  _selectedIndex == widget.items.indexOf(e))
                               ? Icon(widget.selectedIcon ?? Icons.done)
                               : const SizedBox.shrink(),
-                        );
-                      },
-                      child: Text(e.title),
-                    ),
-                  ))
-              .toList(),
-        ),
-      ],
+                        ),
+                      );
+                    },
+                    child: Text(e.title,
+                        style: theme.textTheme.bodyMedium?.copyWith(
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ),
+                ))
+            .toList(),
+      ),
     );
   }
 }
