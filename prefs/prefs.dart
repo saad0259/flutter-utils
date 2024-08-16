9a67cfc37215b0fa3dd0abbb250de65a48a06b5d
Saad Shafiq <saadshafiq259@gmail.com>
2024-08-16 23:38:36 +0500

    need prefs

--- prefs/prefs.dart
+++ prefs/prefs.dart
@@ -1,12 +1,10 @@
-// import '../../preferences/prefs_helper.dart';
+import 'prefs_helper.dart';
 
-// Prefs get prefs {
-//   return Prefs._prefs;
-// }
+Prefs get prefs {
+  return Prefs._prefs;
+}
 
-// class Prefs {
-//   static final _prefs = Prefs();
-//   final token = PrefsHelper<String>("token");
-//   final userId = PrefsHelper<String>("userId");
-//   final selectedLocale = PrefsHelper<String>("selectedLocale");
-// }
+class Prefs {
+  static final _prefs = Prefs();
+  final showNotification = PrefsHelper<bool>("showNotification");
+}
